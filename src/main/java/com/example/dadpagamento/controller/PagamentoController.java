c9c539d6691fb85f6dad7b2bf5b62ff7ed308fcd
igor <quaresmaigor@yahoo.com>
2023-11-25 09:50:50 -0300

    implementando service ecomeço da controller

--- src/main/java/com/example/dadpagamento/controller/PagamentoController.java
+++ src/main/java/com/example/dadpagamento/controller/PagamentoController.java
@@ -1,10 +1,19 @@
 package com.example.dadpagamento.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dadpagamento.repository.PagamentoRepository;
+import com.example.dadpagamento.service.PagamentoService;
 
 @RestController
 @RequestMapping("/pagamento")
 public class PagamentoController {
+
+	@Autowired
+	PagamentoService service;
+	@Autowired
+	PagamentoRepository repository;
 
 }
--- src/main/java/com/example/dadpagamento/service/PagamentoService.java
+++ src/main/java/com/example/dadpagamento/service/PagamentoService.java
@@ -1,8 +1,35 @@
 package com.example.dadpagamento.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.dadpagamento.entidades.Pagamento;
+import com.example.dadpagamento.repository.PagamentoRepository;
 
 @Service
 public class PagamentoService {
+	
+	
+	@Autowired
+	PagamentoRepository repository;
+	
+	
+	public  List<Pagamento> findall(){
+		return repository.findAll();
+	}
+	
+	
+	public Pagamento insert(Pagamento payment) {
+		
+		return repository.save(payment);
+	}
+	
+	public Optional<Pagamento> findbyid(Long id) {
+		Optional<Pagamento> obj= repository.findById(id);
+		return obj;
+	}
 
 }
