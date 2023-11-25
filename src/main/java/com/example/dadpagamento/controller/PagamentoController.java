686df20b1d9a138b0dbe7f694bb4a6ed9d192398
igor <quaresmaigor@yahoo.com>
2023-11-25 10:27:38 -0300

    insert pagamento controller e arrumando atributos

--- src/main/java/com/example/dadpagamento/controller/PagamentoController.java
+++ src/main/java/com/example/dadpagamento/controller/PagamentoController.java
@@ -1,9 +1,14 @@
 package com.example.dadpagamento.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dadpagamento.entidades.Pagamento;
 import com.example.dadpagamento.repository.PagamentoRepository;
 import com.example.dadpagamento.service.PagamentoService;
 
@@ -15,5 +20,26 @@ public class PagamentoController {
 	PagamentoService service;
 	@Autowired
 	PagamentoRepository repository;
+
+	@PostMapping("/realizarpagamento/saldo={saldo}/valorfatura={valor}/data={datafatura}/status={status}")
+	public ResponseEntity insert(@PathVariable float saldo, @PathVariable float valor,
+			@PathVariable String datafatura, @PathVariable String status) {
+		Pagamento payment = new Pagamento();
+		payment.setStatus(status);
+		if(saldo > valor) {
+			float saldoatualizado=saldo-valor;
+			payment.setData(datafatura);
+			payment.setPrecoFatura(valor);
+			payment.setSaldo(saldo);						
+			payment.setSaldoAtualizado(saldoatualizado);
+			payment=service.insert(payment);
+			payment.setStatus("Pago");
+			return ResponseEntity.ok().body(payment);
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body("Saldo Indisponivel");
+		
+	}
 
 }
--- src/main/java/com/example/dadpagamento/entidades/Pagamento.java
+++ src/main/java/com/example/dadpagamento/entidades/Pagamento.java
@@ -1,6 +1,8 @@
 package com.example.dadpagamento.entidades;
 
 import java.io.Serializable;
+import java.time.Instant;
+import java.util.Random;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,17 +21,21 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode
 @Entity
+
 public class Pagamento  implements Serializable {
+	Random randomGenerator = new Random();
 	
-	private static final long serialVersionUID = 1L;
-	
- 
-	
+	private static final long serialVersionUID = 1L;	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	int random = randomGenerator.nextInt(1000000000);
+	private String codigo="CPDS"+random+"HUHASSA-44"+random;
 	private float saldo;
-	private String dataTransacao;
+	private String empresa="@payment-dad";
+	private Instant dataTransacao=Instant.now();
+	private String data;
 	private String status;
 	private float precoFatura;
 	private float saldoAtualizado;
